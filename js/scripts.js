4850336dc9c1857f74a25a3c98ffdf9608182047
Chege <chegekformal@gmail.com>
2017-04-26 16:41:17 +0300

    Tried to add dynamic text. It's not running yet.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,16 @@
 $(document).ready(function() {
-  $("h1").click(function() {
+  $('#start').click(function() {
     var sentence1 = prompt("Please enter a sentence in the provided box.");
     alert("You wrote: " + sentence1);
-    //This first section initializes the variable "sentence1"
-    function capsOn() {
-      var char1 = (sentence1.charAt(0));
-      var char2 = sentence1.charAt(sentence1.length - 1);
+    var char1 = (sentence1.charAt(0));
+    var char2 = sentence1.charAt(sentence1.length - 1);
+    $("h1").after("<p>Now click here...</p>");
+  });
+
+  $('#start').on("click", 'p', function() {
       alert("If we take your first and last characters and capitalise them, we get: ");
       alert(char1.toUpperCase() + char2.toUpperCase());
       alert("When we reverse this, we get: ");
       alert(char2.toUpperCase() + char1.toUpperCase());
-    }
-    capsOn();
+    });
   });
-
-});
